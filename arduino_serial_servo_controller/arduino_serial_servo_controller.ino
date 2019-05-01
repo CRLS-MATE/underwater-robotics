d22c7192d245bdb610799382a74057248eef865a
maxtkc <maxkatzchristy@gmail.com>
2019-05-01 16:23:52 -0400

    cleaned and commented out excess

--- arduino_serial_servo_controller/arduino_serial_servo_controller.ino
+++ arduino_serial_servo_controller/arduino_serial_servo_controller.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 
 int inByte;
-int currentServo = 2;
+int currentServo = 2; // changes based on values sent over serial
 Servo myservos[24];  // create servo object to control a servo
 
 void setup() {
@@ -9,15 +9,14 @@ void setup() {
     pinMode(x, OUTPUT);
   }
 
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(12, HIGH);
-  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH); // Direction pin for A
+  digitalWrite(13, HIGH); // Direction pin for B
   
-  for (int x = 2; x <= 13; x++) {
-    if (!block(x)) {
-      myservos[x - 2].attach(x);  // attaches the servo on pin 9 to the servo object
-    }
+  for (int x = 2; x <= 11; x++) {
+    myservos[x - 2].attach(x);  // attaches the servo on pin 9 to the servo object
+//    if (!block(x)) {
+//      myservos[x - 2].attach(x);  // attaches the servo on pin 9 to the servo object
+//    }
   }
 
   Serial.begin(9600);
@@ -26,21 +25,21 @@ void setup() {
 void loop() {
   if (Serial.available()) {
     inByte = Serial.read();
-    if (inByte >= 180) {
+    if (inByte >= 182 && inByte < 192) { // if it's a reasonable servo value
       inByte -= 180;
-      if(!block(inByte)) {
-        currentServo = inByte;
-        Serial.println(currentServo);
-      }
-    } else {
-      myservos[currentServo].write(inByte);
-      Serial.print(currentServo);
-      Serial.print('\t');
-      Serial.println(inByte);
+      currentServo = inByte;
+//      if(!block(inByte)) {
+//        Serial.println(currentServo);
+//      }
+    } else if(inByte <= 180) {
+      myservos[currentServo - 2].write(inByte);
+//      Serial.print(currentServo);
+//      Serial.print('\t');
+//      Serial.println(inByte);
     }
   }
 }
 
-bool block(int pin) { // special pins for motor shield
-  return (pin == 8 || pin == 9 || pin == 12 || pin == 13);
-}
+//bool block(int pin) { // special pins for motor shield
+//  return (pin == 12 || pin == 13);
+//}
